8f921c559c828b6332c4e9d2d4d846505fba1bb9
vd1516 <vrushabh.dhameliya1516@gmail.com>
2024-08-28 14:18:45 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //To Do State
+  let [toDoList, setTodoList] = useState([])
+
+  //Save To do
+  let saveTodoList = (event) => {
+    let toDoName = event.target.toname.value
+    if (toDoList.includes(toDoName)) {
+      alert('This To Do List Already Exist')
+    } else {
+      let finalTodoList = [...toDoList, toDoName]
+      setTodoList(finalTodoList)
+      console.log(finalTodoList)
+      console.log("Done")
+    }
+
+    event.preventDefault()
+  }
+
+  //Show to do
+  let showTodo = toDoList.map((value, index) => {
+    return (
+      <ToDolistItems value={value} key={index} indexNumber={index}
+        todoList={toDoList} setTodoList={setTodoList} />
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Add To Do */}
+      <h1>To do list</h1>
+      <form onSubmit={saveTodoList}>
+        <input type='text' placeholder='Enter to do' name='toname' />
+        <button>Save</button>
+      </form>
+
+      {/* Show To Do */}
+      <div className='outerDiv'>
+        <ul>
+          {showTodo}
+        </ul>
+      </div>
+
     </div>
   );
 }
 
+function ToDolistItems({ value, indexNumber, todoList, setTodoList }) {
+  //Line on content
+  let [status,setStatus] = useState(false)
+
+  let deleteRow = () => {
+    let finalData = todoList.filter((v,i) => i != indexNumber)
+    setTodoList(finalData)
+  }
+
+  
+
+  return (
+    <li className={(status) ? 'completetodo' : ''} onClick={() => setStatus(!status)}>{value} <span onClick={deleteRow}>&times;</span></li>
+  )
+}
 export default App;
